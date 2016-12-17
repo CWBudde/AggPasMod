f58a357dfd9a71d01a754cd6e0212817f95bba69
Christian <Christian@savioursofsoul.de>
2016-12-17 16:53:53 +0100

    Updated copyright year

--- Examples/VCL/Agg2D/MainUnit.pas
+++ Examples/VCL/Agg2D/MainUnit.pas
@@ -732,16 +732,16 @@ begin
     FlipText := True;
 
     Font('Arial', 16, False, False, fcRaster);
-    Text(0, 16, WrapText('Lorem ipsum dolor sit amet, consetetur sadipscing '
-      + 'elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore '
-      + 'magna aliquyam erat, sed diam voluptua. At vero eos et accusam et '
-      + 'justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea '
+    Text(0, 16, AnsiString(WrapText('Lorem ipsum dolor sit amet, consetetur '
+      + 'sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et '
+      + 'dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam '
+      + 'et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea '
       + 'takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor '
       + 'sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor '
       + 'invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. '
       + 'At vero eos et accusam et justo duo dolores et ea rebum. Stet clita '
       + 'kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit '
-      + 'amet.', #13#10, ['.',' ',#9,'-'], 80));
+      + 'amet.', #13#10, ['.',' ',#9,'-'], 80)));
   end;
 
   Agg2DControl.Invalidate;
@@ -922,4 +922,3 @@ begin
 end;
 
 end.
-
